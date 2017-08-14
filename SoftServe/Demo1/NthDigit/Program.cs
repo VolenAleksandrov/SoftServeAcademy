93ccd40618e4270d14988663cd050c2b12029248
VolenAleksandrov <vo13nn@gmail.com>
2017-08-14 16:04:23 +0300

    Add Tasks

---
+++ SoftServe/Demo1/NthDigit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NthDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int digit = IntPow(n, n);
+
+            if (digit.ToString().Length < n)
+            {
+                Console.WriteLine("*");
+            }
+            else
+            {
+                int digitLength = digit.ToString().Length;
+                Console.WriteLine(digit.ToString()[digitLength - n]);
+            }
+        }
+        public static int IntPow(int number, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+    }
+}
